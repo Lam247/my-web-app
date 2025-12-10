fd90916290075202ba82201e14c99ac0aa07715a
lam247 <cualamthuido@gmail.com>
2025-12-10 20:46:57 +0700

    update code lan 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Xin chao Ubuntu! Day la web Node.js cua toi!</h1>');
+  res.send('<h1>Xin chao Ubuntu! Toi la Lam</h1>');
 });
 
 app.listen(port, () => {
