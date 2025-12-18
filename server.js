7e7e49e4f555905936a346b184ffefb8e4da8094
lam247 <cualamthuido@gmail.com>
2025-12-18 07:34:05 +0700

    Test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Xin chao Ubuntu! Toi la Lam - </h1><h1>Tran Kiem Lam - DH52200971</h1>');
+  res.send('<h1>Tran Kiem Lam - DH52200971</h1>');
 });
 
 app.listen(port, () => {
